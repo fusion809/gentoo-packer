9bc40853983453df1c411ce7d756133bb395aeca
Brenton Horne <brentonhorne77@gmail.com>
2016-04-07 19:29:14 +1000

    Update stage3.sh

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -5,4 +5,4 @@ tarball=stage3-amd64-nomultilib-$STAGE3.tar.bz2
 mount /dev/sda4 /mnt/gentoo
 
 cd /mnt/gentoo
-curl -L http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3/$tarball | tar xvjp
+curl -sL http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3/$tarball | tar xvjp
