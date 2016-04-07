031633eb97357f8da14752e9d3337078d3c24874
Brenton Horne <brentonhorne77@gmail.com>
2016-04-07 19:23:15 +1000

    Update timezone.sh

--- scripts/timezone.sh
+++ scripts/timezone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
-ln -snf /usr/share/zoneinfo/UTC /etc/localtime
-echo UTC > /etc/timezone
+ln -snf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
+echo "Australia/Brisbane" > /etc/timezone
 EOF
