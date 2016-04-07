45da30908455b115bfb3d380ae5aac12bf70b6bc
Brenton Horne <brentonhorne77@gmail.com>
2016-04-07 22:43:37 +1000

    Update virtualbox.sh

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -5,9 +5,9 @@ emerge "=virtual/linux-sources-1" --autounmask-write
 etc-update --automode -5
 emerge "=virtual/linux-sources-1"
 
-emerge ">=app-emulation/virtualbox-guest-additions-$VBGA" --autounmask-write
+emerge ">=app-emulation/virtualbox-guest-additions-5.0.16" --autounmask-write
 etc-update --automode -5
-emerge ">=app-emulation/virtualbox-guest-additions-$VBGA"
+emerge ">=app-emulation/virtualbox-guest-additions-5.0.16"
 
 rc-update add virtualbox-guest-additions default
 EOF
