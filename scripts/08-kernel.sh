46ee03e8ce17e143da10d0b7a0ee328483b5c700
fusion809 <brentonhorne77@gmail.com>
2016-04-16 01:22:49 +1000

    updating scripts

--- scripts/08-kernel.sh
+++ scripts/08-kernel.sh
@@ -3,6 +3,8 @@
 cp $SCRIPTS/scripts/kernel.config /mnt/gentoo/tmp/
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
+emerge --autounmask-write ">=sys-kernel/gentoo-sources-4.5.1"
+etc-update --automode -5
 emerge ">=sys-kernel/gentoo-sources-4.5.1"
 emerge sys-kernel/genkernel
 cd /usr/src/linux
