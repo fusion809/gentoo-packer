0aef920585eb6de92b02cca4b91a7e8b1696efcd
fusion809 <brentonhorne77@gmail.com>
2016-04-16 02:56:10 +1000

    updating scripts

--- scripts/10-virtualbox.sh
+++ scripts/10-virtualbox.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
-emerge "=virtual/linux-sources-1" --autounmask-write
-etc-update --automode -5
 emerge "=virtual/linux-sources-1"
-
 emerge ">=app-emulation/virtualbox-guest-additions-5.0.16" --autounmask-write
 etc-update --automode -5
 emerge ">=app-emulation/virtualbox-guest-additions-5.0.16"
