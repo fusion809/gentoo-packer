755b453df2cc0df7ddeb1d05563093fa71a655a6
fusion809 <brentonhorne77@gmail.com>
2016-04-16 02:19:28 +1000

    updating scripts

--- scripts/05-portage.sh
+++ scripts/05-portage.sh
@@ -3,4 +3,6 @@
 chroot /mnt/gentoo /bin/bash <<'EOF'
 mkdir /usr/portage
 emerge-webrsync
+sed -i -e 's/USE="bindist mmx sse sse2"/USE="-bindist mmx sse sse2"/g' /etc/portage/make.conf
+echo 'ACCEPT_KEYWORDS="~amd64"' >> /etc/portage/make.conf
 EOF
