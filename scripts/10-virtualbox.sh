a6c8d102d7006c4e6be95e7d44c4bcfd4138b5af
Brenton Horne <brentonhorne77@gmail.com>
2016-05-16 23:34:05 +1000

    Update 10-virtualbox.sh

--- scripts/10-virtualbox.sh
+++ scripts/10-virtualbox.sh
@@ -2,9 +2,9 @@
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
 emerge "=virtual/linux-sources-1"
-emerge ">=app-emulation/virtualbox-guest-additions-5.0.16" --autounmask-write
+emerge ">=app-emulation/virtualbox-guest-additions-5.0.20" --autounmask-write
 etc-update --automode -5
-emerge ">=app-emulation/virtualbox-guest-additions-5.0.16"
+emerge ">=app-emulation/virtualbox-guest-additions-5.0.20"
 
 rc-update add virtualbox-guest-additions default
 EOF
